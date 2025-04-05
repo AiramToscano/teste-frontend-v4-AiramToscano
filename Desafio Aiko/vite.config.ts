c75ec0a827c4f84791950500bee4ec14662bd681
airamtoscano <airam.almeida@ipdec.org>
2025-04-05 13:05:07 -0400

    fix: adding .env

--- Desafio Aiko/vite.config.ts
+++ Desafio Aiko/vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), 'VITE_APP_')
+  return {
+    plugins: [vue()],
+    server: {
+      port: +env.VITE_APP_PORT,
+    },
+  }
 })
